a17c7869f1e2b4e529be4bfdc2756a4d0d9257a3
REGISTROCIVIL\dario.cardenas <dario.cardenas@registrocivil.gob.ec>
2023-08-18 08:55:56 -0500

    Cloud Computing Exam V1.0: geolocations

--- EXAM/SaludoUteg.java
+++
@@ -1,7 +0,0 @@
-public class SaludoUteg{
-	public static void main(String args[]({
-        System.out.println(" Esto es un archivo de Bienvenida para todos los compañeros... ");
-	System.out.println(" Esta es la carpeta creada para el EXAMEN DE LA MAESTRIA ");
-	System.out.println(" Cada compañero debe crear una carpeta con su nombre y guardar los 2 archivos que solicitan... ");
-	}
-}
